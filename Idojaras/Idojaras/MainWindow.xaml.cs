d72b9c2e356ec02d44b1e6b0b56b67edd00cba6e
E-Looper-Dev <134919213+E-Looper-Dev@users.noreply.github.com>
2024-03-19 11:00:17 +0100

    Add files via upload

---
+++ Idojaras/Idojaras/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System;
+using System.Collections.Generic;
+
+namespace Idojaras
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+		List<List<string>> idojarasLista = Read_File(@"..\..\..\..\idojaras.txt");
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private static List<List<string>> Read_File(string filePath)
+		{
+			List<List<string>> idojarasLista = new List<List<string>>();
+
+			try
+			{
+				// Fájl sorainak beolvasása és feldolgozása
+				using (StreamReader sr = new StreamReader(filePath))
+				{
+					string sor;
+					while ((sor = sr.ReadLine()) != null)
+					{
+						// Sor szétválasztása városra és időjárásra+hőmérsékletre
+						string[] sorReszek = sor.Split(' ');
+						string varos = sorReszek[0];
+						string idojaras = sorReszek[1] + " " + sorReszek[2];
+
+						// 2 elemű lista létrehozása és hozzáadása a fő listához
+						idojarasLista.Add(new List<string> { varos, idojaras });
+					}
+				}
+
+				return idojarasLista;
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show("Hiba történt a fájl olvasása közben: " + e.Message);
+				return idojarasLista;
+			}
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			string varos = text_box.Text;
+			int i = 0;
+            while (i < idojarasLista.Count && idojarasLista[i][0] == varos)  
+            {
+				i++;
+            }
+			if (i < idojarasLista.Count)
+			{
+				MessageBox.Show(idojarasLista[0] + idojarasLista[i][1]);
+			}
+			else
+			{
+				MessageBox.Show("Nincs ilyen város.");
+			}
+			MessageBox.Show(varos);
+        }
+	}
+}
---
+++ Tomi Gyalázó/Tomi Gyalázó/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tomi_Gyalázó
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		string next = "X";
+		List<Button> Buttons = new List<Button>();
+		public MainWindow()
+		{
+			InitializeComponent();
+			InitializeButtons();
+		}
+		private void InitializeButtons()
+		{
+			for (int sor = 0; sor < 3; sor++)
+			{
+				for(int oszlop = 0; oszlop < 3; oszlop++)
+				{
+					Button button = new Button();
+					button.Click += Button_Click_Event;
+					button.FontSize = 30;
+					button.Content = "";
+					Grid.SetRow(button, sor);
+					Grid.SetColumn(button, oszlop);
+					GameGrid.Children.Add(button);
+					Buttons.Add(button);
+				}
+			}
+		}
+		private void Button_Click_Event(object sender, RoutedEventArgs e)
+		{
+			Button button = sender as Button;
+			button.Content = next;
+			ChangeEvent();
+		}
+		private void ChangeEvent()
+		{
+			if (next == "X")
+			{
+				next = "0";
+			}
+			else
+			{
+				next = "X";
+			}
+		}
+		private void Set(Button button)
+		{
+			if (button.Content == "")
+			{
+				button.Content = next;
+				ChangeEvent();
+			}
+		}
+	}
+}
